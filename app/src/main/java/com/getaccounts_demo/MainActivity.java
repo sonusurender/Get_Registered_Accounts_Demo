157786b6655ee6a6c62202c322673948d6fe64c9
Surender Kumar <sonu.surendra0@gmail.com>
2020-09-14 14:58:19 +0530

    fixed runtime permission issue to fetch accounts

--- app/src/main/java/com/getaccounts_demo/MainActivity.java
+++ app/src/main/java/com/getaccounts_demo/MainActivity.java
@@ -1,14 +1,22 @@
 package com.getaccounts_demo;
 
+import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static TextView googleAccounts, allAccounts;
+
+    private static final int REQUEST_ACCOUNTS_PERMISSION = 1;
+    private TextView googleAccounts, allAccounts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,27 @@ public class MainActivity extends AppCompatActivity {
         googleAccounts = (TextView) findViewById(R.id.googleAccounts);
         allAccounts = (TextView) findViewById(R.id.allAccounts);
 
+        requestGetAccountsPermission();
+    }
 
+    public void requestGetAccountsPermission() {
+        //Check if permission is granted or not
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.GET_ACCOUNTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.GET_ACCOUNTS},
+                    REQUEST_ACCOUNTS_PERMISSION);
+        } else {
+            fetchAccounts();
+        }
+
+    }
+
+    /**
+     * method to fetch all device accounts
+     */
+    private void fetchAccounts() {
         //Get all Google Accounts
         try {
             String googleAccount = "";
@@ -53,4 +81,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_ACCOUNTS_PERMISSION:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // task you need to do.
+                    fetchAccounts();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
 }
